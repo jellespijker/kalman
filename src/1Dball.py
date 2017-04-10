e1a6e66fc943be3990751c4bc03ca080b24a2294
Jelle Spijker <spijker.jelle@gmail.com>
2017-04-10 12:42:17 +0200

    1D and 2D updated, working but needs fine tuning for the process

--- src/1Dball.py
+++ src/1Dball.py
@@ -1,27 +1,29 @@
 import matplotlib.pyplot as pl
-from mpl_toolkits.mplot3d import Axes3D
+
 pl.rcParams['legend.loc'] = 'best'
 import numpy as np
+from numpy import dot
 
 
 def build_real_values():
-    num_of_time_steps = 702
+    num_of_time_steps = 100
     dt = 0.2
     t = np.linspace(start=0., stop=num_of_time_steps * dt, num=num_of_time_steps)
-    s = np.zeros((num_of_time_steps, 2))
-    v = np.zeros((num_of_time_steps, 2))
-    a = np.zeros((num_of_time_steps, 2))
+    s = np.zeros((num_of_time_steps, 1))
+    v = np.zeros((num_of_time_steps, 1))
+    a = np.zeros((num_of_time_steps, 1))
 
-    a = np.ones((num_of_time_steps, 1)) * 9.81 # Build accelaration profile
-    v = np.cumsum(a, axis=0)  # integrate to gain velocity
-    s = np.cumsum(v, axis=0)  # integrate to gain position
+    a = np.ones((num_of_time_steps, 1)) * 9.81  # Build accelaration profile
+    for i in range(1, num_of_time_steps):
+        v[i] = v[i - 1] + a[i] * dt  # Build speed profile
+        s[i] = s[i - 1] + v[i] * dt + 0.5 * a[i] * dt ** 2  # Build position profile
 
     return [t, dt, s, v, a]
 
 
 def build_measurement_values(t, S):
     y = np.zeros((t.size, 2, 1))
-    S_m = S * np.random.normal(1, 0.02, (1, len(S))).transpose()
+    S_m = np.random.normal(1, 20, (len(S), 1)) + S
     y[:, 0] = S_m
     return y
 
@@ -32,36 +34,38 @@ def build_control_values(t, a):
 
 
 def init_kalman(t, dt):
+    phi_s = 5e-3
+
     F = np.array([
-        [1., 0.],
+        [1., dt],
         [0., 1.]
     ])
 
     B = np.array([
-        [dt, 0.5 * dt ** 2],
+        [0.5 * dt ** 2, 0.],
         [0, dt]
     ])
 
     H = np.array([
         [1., 0.],
-        [0., 0.]
+        [0., 0.0]
     ])
 
     Q = np.array([
-        [1., .1],
-        [.1, 1.]
-    ])
+        [(1 / 3) * dt ** 3, 0.5 * dt ** 2],
+        [0.5 * dt ** 2, dt]
+    ]) * phi_s
 
     R = np.array([
-        [1.2, 0.2],
-        [0.2, 0.2]
+        [5.0, 0.0],
+        [0.0, 5.0e-9]
     ])
     v = np.random.normal(0, 25e-3, (t.size, 2, 1))
     w = np.random.normal(0, 25e-3, (t.size, 2, 1))
     return [F, B, H, Q, R, v, w]
 
 
-def kalman(t, kalman_values, u, y, error):
+def kalman(t, kalman_values, u, z, error):
     x = np.zeros((t.size, 2, 1))
     P = np.zeros((t.size, 2, 2))
     P[0, :, :] = np.array([
@@ -70,7 +74,7 @@ def kalman(t, kalman_values, u, y, error):
     ])
 
     xhat = np.zeros((t.size, 2, 1))
-    z = np.zeros((t.size, 2, 1))
+    y = np.zeros((t.size, 2, 1))
 
     F = kalman_values[0]
     B = kalman_values[1]
@@ -83,65 +87,63 @@ def kalman(t, kalman_values, u, y, error):
     K = np.zeros((t.size, 2, 2))
 
     for k in range(1, t.size):
-        # predict state
-        xhat[k] = F.dot(x[k - 1]) + B.dot(u[k]) + w[k]
-        P[k] = F.dot(P[k - 1]).dot(F.transpose()) + Q
+        xhat[k] = dot(F, x[k - 1]) + dot(B, u[k]) + w[k]
+        Phat = dot(F, dot(P[k - 1], F.T)) + Q
 
-        # Measurement
-        z[k] = H.dot(y[k]) + v[k]
+        y[k] = z[k] - dot(H.T, xhat[k])
 
-        # Update and calculate gain
-        K[k] = P[k].dot(H.transpose()).dot(np.linalg.inv(H.dot(P[k]).dot(H.transpose()) + R))
-        x[k] = xhat[k] + K[k].dot(z[k] - H.dot(xhat[k]))
+        S = dot(H.T, dot(Phat, H)) + R
+        S = np.linalg.inv(S)
+        K[k] = dot(Phat, dot(H, S))
+        x[k] = xhat[k] + dot(K[k], y[k])
 
-        # current state
-        P[k] = (np.eye(2) - K[k].dot(H)).dot(P[k])
+        P[k] = dot(np.eye(2) - dot(K[k], H.T), Phat)
     return [x, K, P, xhat, z]
 
 
 def plot_results(t, x, s, v, a, u, y, K, P, xhat, z):
-    x_ = np.linspace(0, P.shape[1] - 1, P.shape[1])
-    y_ = np.linspace(0, P.shape[1] - 1, P.shape[1])
-    X_, Y_ = np.meshgrid(x_, y_)
-
-    for i in range(0, 10, 1):
-        fig = pl.figure()
-        ax = fig.gca(projection='3d')
-        surf = ax.plot_surface(X_, Y_, K[i], linewidth=1, antialiased=True)
-        pl.tight_layout()
-        pl.show()
-
-    s_err = np.zeros((t.size, 1))
-    v_err = np.zeros((t.size, 1))
-    for i in range(1, t.size):
-        s_err[i] = 1. - (s[i] / x[i, 0])
-        v_err[i] = 1. - (v[i] / x[i, 1])
+    res_s = np.reshape(s[:, 0], (t.size, 1)) - x[:, 0]
+    res_v = np.reshape(v[:, 0], (t.size, 1)) - x[:, 1]
 
     pl.figure()
     pl.subplot(311)
     pl.plot(s)
-    pl.plot(xhat[:,0],'o')
-    pl.plot(x[:,0])
-    pl.plot(y[:,0],'.')
+    pl.plot(xhat[:, 0], 'o')
+    pl.plot(x[:, 0])
+    pl.plot(y[:, 0], '.')
     pl.subplot(312)
     pl.plot(v)
-    pl.plot(xhat[:,1],'o')
-    pl.plot(x[:,1])
+    pl.plot(xhat[:, 1], 'o')
+    pl.plot(x[:, 1])
     pl.subplot(313)
-    pl.plot(s_err)
-    pl.plot(v_err)
+    pl.plot(res_s)
+    pl.plot(res_v)
     pl.tight_layout()
+    pl.savefig('fallingBall.png')
     pl.show()
+
+    # save csv
+    datPos = np.zeros((t.size, 3, 1))
+    datPos[:, 0] = s
+    datPos[:, 1] = z[:, 0]
+    datPos[:, 2] = y[:, 0]
+    np.savetxt('fallingBallPos.dat', datPos, delimiter=',')
+
+    datSpeed = np.zeros((t.size, 2, 1))
+    datSpeed[:, 0] = v
+    datSpeed[:, 1] = x[:, 1]
+    np.savetxt('fallingBallSpeed.dat', datSpeed, delimiter=',')
+
 
 def main():
     [t, dt, s, v, a] = build_real_values()
-    y = build_measurement_values(t, s)
+    z = build_measurement_values(t, s)
     u = build_control_values(t, a)
     [F, B, H, Q, R, vv, w] = init_kalman(t, dt)
-    error = [0.25, 2.5]
+    error = [2.5, 2.5]
     kalman_values = [F, B, H, Q, R, vv, w]
-    x, K, P, xhat, z = kalman(t, kalman_values, u, y, error)
-    plot_results(t, x, s, v, a, u, y, K, P, xhat, z)
+    x, K, P, xhat, y = kalman(t, kalman_values, u, z, error)
+    plot_results(t, x, s, v, a, u, y, K, P, xhat, y)
 
 
 if __name__ == '__main__':
--- src/2DBot.py
+++ src/2DBot.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as pl
-from mpl_toolkits.mplot3d import Axes3D
+
 pl.rcParams['legend.loc'] = 'best'
 import numpy as np
+from numpy import dot
 
 
 def build_real_values():
@@ -31,8 +32,13 @@ def build_real_values():
     a_tan[650:700, 0] = -np.linspace(a_tan[600, 0], 0., num=50)
     a_tan[650:700, 1] = -np.linspace(a_tan[600, 1], 0., num=50)
 
-    v = np.cumsum(a_tan, axis=0)  # integrate to gain velocity
-    s = np.cumsum(v, axis=0)  # integrate to gain position
+    for i in range(1, num_of_time_steps):
+        v[i] = v[i - 1] + a_tan[i] * dt
+        s[i] = s[i - 1] + v[i] * dt + 0.5 * a[i] * dt ** 2
+
+    # v = np.cumsum(a_tan, axis=0)  # integrate to gain velocity
+    # s = np.cumsum(v, axis=0)  # integrate to gain position
+    v[0] = 0.1
     theta = np.tanh(np.divide(v[:, 1], v[:, 0]))  # obtain heading from ds or v
     omega = np.diff(theta, axis=0)  # obtain omega from differentiating theta
     alpha = np.diff(omega, axis=0)  # obtain alpha from differentiating omega
@@ -66,76 +72,82 @@ def build_measurement_values(t, real_values):
     drift = np.ones((real_values[1].size,)) * np.random.normal(drift_mu, drift_sigma)
     omega_m += drift
 
-    y = np.zeros((t.size, 9, 1))
-    y[:, 4] = np.reshape(a_m[:, 0], (t.size, 1))
-    y[:, 5] = np.reshape(a_m[:, 1], (t.size, 1))
-    y[:, 7] = np.reshape(omega_m, (t.size, 1))
-    return y
+    z = np.zeros((t.size, 3, 1))
+    z[:, 0] = np.reshape(a_m[:, 0], (t.size, 1))
+    z[:, 1] = np.reshape(a_m[:, 1], (t.size, 1))
+    z[:, 2] = np.reshape(omega_m, (t.size, 1))
+    return z
 
 
 def build_control_values(t, real_values):
-    u = np.zeros((t.size, 9, 1))
-    u[:, 2] = np.reshape(real_values[:, 0], (t.size, 1))
-    u[:, 3] = np.reshape(real_values[:, 1], (t.size, 1))
+    u = np.zeros((t.size, 2, 1))
+    u[:, 0] = np.reshape(real_values[:, 0], (t.size, 1))
+    u[:, 1] = np.reshape(real_values[:, 1], (t.size, 1))
     return u
 
 
 def init_kalman(t, dt):
+    phi_s = 1.
+
     F = np.array([
-        [1., 0., 0., 0., 0.5 * dt ** 2, 0., 0., 0., 0.],
-        [0., 1., 0., 0., 0., 0.5 * dt ** 2, 0., 0., 0.],
-        [0., 0., 0., 0., 0., 0., 0., 0., 0.],
-        [0., 0., 0., 0., 0., 0, 0., 0., 0.],
-        [0., 0., 0., 0., 1., 0., 0., 0., 0.],
-        [0., 0., 0., 0., 0., 1., 0., 0., 0.],
-        [0., 0., 0., 0, 0., 0., 1., dt, 0.5 * dt ** 2],
-        [0., 0., 0., 0, 0., 0., 0., 1., dt],
-        [0., 0., 0., 0, 0., 0., 0., 0., 1]
+        [1., 0., 0.5 * dt ** 2, 0., 0., 0., 0., 0., 0.],
+        [0., 0., dt, 0., 0., 0., 0., 0., 0.],
+        [0., 0., 1., 0., 0., 0., 0., 0., 0.],
+        [0., 0., 0., 1., 0., 0.5 * dt ** 2, 0., 0., 0.],
+        [0., 0., 0., 0., 0., dt, 0., 0., 0.],
+        [0., 0., 0., 0., 0., 1., 0., 0., 0., ],
+        [0., 0., 0., 0., 0., 0., 1., dt, 0.5 * dt ** 2],
+        [0., 0., 0., 0., 0., 0., 0., 1., dt],
+        [0., 0., 0., 0., 0., 0., 0., 0., 1.]
     ])
 
     B = np.array([
-        [0., 0., dt, 0., 0., 0., 0., 0., 0.],
-        [0., 0., 0., dt, 0., 0., 0., 0., 0.],
-        [0., 0., 1., 0., 0., 0., 0., 0., 0.],
-        [0., 0., 0., 1., 0., 0., 0., 0., 0.],
-        [0., 0., 0., 0., 0., 0., 0., 0., 0.],
-        [0., 0., 0., 0., 0., 0., 0., 0., 0.],
-        [0., 0., 0., 0., 0., 0., 0., 0., 0.],
-        [0., 0., 0., 0., 0., 0., 0., 0., 0.],
-        [0., 0., 0., 0., 0., 0., 0., 0., 0.]
+        [dt, 0.],
+        [1., 0.],
+        [0., 0.],
+        [0., dt],
+        [0., 1.],
+        [0., 0.],
+        [0., 0.],
+        [0., 0.],
+        [0., 0.]
     ])
 
     H = np.array([
-        [0., 0., 0., 0., 0., 0., 0., 0., 0.],
-        [0., 0., 0., 0., 0., 0., 0., 0., 0.],
-        [0., 0., 0., 0., 0., 0., 0., 0., 0.],
-        [0., 0., 0., 0., 0., 0., 0., 0., 0.],
-        [0., 0., 0., 0., 1., 0., 0., 0., 0.],
-        [0., 0., 0., 0., 0., 1., 0., 0., 0.],
-        [0., 0., 0., 0., 0., 0., 0., 0., 0.],
-        [0., 0., 0., 0., 0., 0., 0., 1., 0.],
-        [0., 0., 0., 0., 0., 0., 0., 0., 0.]
+        [0., 0., 0.],
+        [0., 0., 0.],
+        [1., 0., 0.],
+        [0., 0., 0.],
+        [0., 0., 0.],
+        [0., 1., 0.],
+        [0., 0., 0.],
+        [0., 0., 1.],
+        [0., 0., 0]
     ])
 
-    Q = np.diag([2, 2, 0, 0, 0.25, 0.25, 1, 1, 1])
+    Q = np.array([
+        [0.05 * dt ** 5, 0., 0.1666 * dt ** 3, 0., 0., 0., 0., 0., 0.],
+        [0., 0., 0., 0., 0., 0., 0., 0., 0.],
+        [0.1666 * dt ** 3, 0., dt, 0., 0., 0., 0., 0., 0.],
+        [0., 0., 0., 0.05 * dt ** 5, 0., 0.1666 * dt ** 3, 0., 0., 0.],
+        [0., 0., 0., 0., 0., 0., 0., 0., 0.],
+        [0., 0., 0., 0.1666 * dt ** 3, 0., dt, 0., 0., 0.],
+        [0., 0., 0., 0., 0., 0., 0.05 * dt ** 5, 0.125 * dt ** 4, 0.1666 * dt ** 3],
+        [0., 0., 0., 0., 0., 0., 0.125 * dt ** 4, 0.333 * dt ** 3, 0.5 * dt ** 2],
+        [0., 0., 0., 0., 0., 0., 0.1666 * dt ** 3, 0.5 * dt ** 2, dt]
+    ]) * phi_s
 
     R = np.array([
-        [1., 0., 0., 0., 0., 0., 0., 0., 0.],
-        [0., 1., 0., 0., 0., 0., 0., 0., 0.],
-        [0., 0., 0.0025, 0., 0., 0., 0., 0., 0.],
-        [0., 0., 0., 0.0025, 0., 0., 0., 0., 0.],
-        [0., 0., 0., 0., 0.25, 0., 0., 0., 0.],
-        [0., 0., 0., 0., 0., 0.25, 0., 0., 0.],
-        [0., 0., 0., 0., 0., 0., 1., 0., 0.],
-        [0., 0., 0., 0., 0., 0., 0., 0.5, 0.],
-        [0., 0., 0., 0., 0., 0., 0., 0., 1.]
+        [0., 0., 0.],
+        [0., 0., 0.],
+        [0., 0., 0.],
     ])
     v = np.random.normal(0, 25e-3, (t.size, 9, 1))
     w = np.random.normal(0, 25e-3, (t.size, 9, 1))
     return [F, B, H, Q, R, v, w]
 
 
-def kalman(t, kalman_values, u, y, error):
+def kalman(t, kalman_values, u, z, error):
     x = np.zeros((t.size, 9, 1))
     P = np.zeros((t.size, 9, 9))
     P[0, :, :] = np.array([
@@ -151,7 +163,7 @@ def kalman(t, kalman_values, u, y, error):
     ])
 
     xhat = np.zeros((t.size, 9, 1))
-    z = np.zeros((t.size, 9, 1))
+    y = np.zeros((t.size, 3, 1))
 
     F = kalman_values[0]
     B = kalman_values[1]
@@ -161,77 +173,68 @@ def kalman(t, kalman_values, u, y, error):
     v = kalman_values[5]
     w = kalman_values[6]
 
-    K = np.zeros((t.size, 9, 9))
+    K = np.zeros((t.size, 9, 3))
 
     for k in range(1, t.size):
-        # predict state
-        xhat[k] = F.dot(x[k - 1]) + B.dot(u[k]) + w[k]
-        P[k] = F.dot(P[k - 1]).dot(F.transpose()) + Q
+        xhat[k] = dot(F, x[k - 1]) + dot(B, u[k]) + w[k]
+        Phat = dot(F, dot(P[k - 1], F.T)) + Q
 
-        # Measurement
-        z[k] = H.dot(y[k]) + v[k]
+        y[k] = z[k] - dot(H.T, xhat[k])
 
-        # Update and calculate gain
-        K[k] = P[k].dot(H.transpose()).dot(np.linalg.inv(H.dot(P[k]).dot(H.transpose()) + R))
-        x[k] = xhat[k] + K[k].dot(z[k] - H.dot(xhat[k]))
+        S = dot(H.T, dot(Phat, H)) + R
+        S = np.linalg.inv(S)
+        K[k] = dot(Phat, dot(H, S))
+        x[k] = xhat[k] + dot(K[k], y[k])
 
-        # current state
-        P[k] = (np.eye(9) - K[k].dot(H)).dot(P[k])
-    return [x, K, P, xhat, z]
+        P[k] = dot(np.eye(9) - dot(K[k], H.T), Phat)
+    return [x, K, P, xhat, y]
 
 
 def plot_results(t, x, s, v, a, theta, omega, alpha, y, K, P, xhat, z):
-    x_ = np.linspace(0, P.shape[1] - 1, P.shape[1])
-    y_ = np.linspace(0, P.shape[1] - 1, P.shape[1])
-    X_, Y_ = np.meshgrid(x_, y_)
+    res_s = np.reshape(s[:, 0], (t.size, 1)) - x[:, 0]
+    res_v = np.reshape(v[:, 0], (t.size, 1)) - x[:, 1]
+    res_a = np.reshape(a[:, 0], (t.size, 1)) - x[:, 2]
 
-    for i in range(0, 700, 50):
-        fig = pl.figure()
-        ax = fig.gca(projection='3d')
-        surf = ax.plot_surface(X_, Y_, P[i], rstride=1, cstride=1, cmap='hot', linewidth=0, antialiased=False)
-        pl.tight_layout()
-        pl.show()
-
-    s_err = np.zeros((t.size, 1))
-    v_err = np.zeros((t.size, 1))
-    a_err = np.zeros((t.size, 1))
-    for i in range(1, t.size):
-        s_err[i] = 1. - (s[i, 0] / x[i, 0])
-        v_err[i] = 1. - (v[i, 0] / x[i, 2])
-        a_err[i] = 1. - (a[i, 0] / x[i, 4])
     pl.figure(1)
     pl.subplot(411)
     pl.plot(t, a[:, 0], label='a')
-    pl.plot(t, x[:, 4], 'x', label='x')
-    pl.plot(t, xhat[:, 4], '.', label='xhat')
+    pl.plot(t, x[:, 2], 'x', label='x')
+    pl.plot(t, y[:, 0], '.', label='y')
+    pl.xlabel('Time [s]')
+    pl.ylabel('A_x [m/s^2]')
     pl.legend()
     pl.subplot(412)
     pl.plot(t, v[:, 0], label='v')
-    pl.plot(t, x[:, 2], 'x', label='x')
-    pl.plot(t, xhat[:, 2], '.', label='xhat')
+    pl.plot(t, x[:, 1], 'x', label='x')
+    pl.xlabel('Time [s]')
+    pl.ylabel('v_x [m/s]')
     pl.legend()
     pl.subplot(413)
-    pl.plot(t, s[:, 0], label='s')
-    pl.plot(t, x[:, 0], 'x', label='x')
-    pl.plot(t, xhat[:, 2], '.', label='xhat')
+    pl.plot(s[:, 0], s[:, 1], label='s')
+    pl.plot(x[:, 0], x[:, 3], 'x', label='x')
+    pl.ylabel('y [m]')
+    pl.xlabel('x [m]')
     pl.legend()
     pl.subplot(414)
-    pl.plot(t, s_err, label='s_err')
-    pl.plot(t, v_err, label='v_err')
-    pl.plot(t, a_err, label='a_err')
+    pl.plot(t, res_s, label='residual s')
+    pl.plot(t, res_v, label='residual v')
+    pl.plot(t, res_a, label='residual a')
+    pl.xlabel('Time [s]')
+    pl.ylabel('residual')
     pl.legend()
     pl.tight_layout()
+    pl.savefig('2Dbot.png')
     pl.show()
 
 
 def main():
     [t, dt, s, v, a, theta, omega, alpha] = build_real_values()
-    y = build_measurement_values(t, [a, omega])
+    z = build_measurement_values(t, [a, omega])
     u = build_control_values(t, v)
     [F, B, H, Q, R, vv, w] = init_kalman(t, dt)
     error = [2, 0.5, 0.5, 0.5, 0.2, 0.4]
     kalman_values = [F, B, H, Q, R, vv, w]
-    x, K, P, xhat, z = kalman(t, kalman_values, u, y, error)
+    x, K, P, xhat, y = kalman(t, kalman_values, u, z, error)
     plot_results(t, x, s, v, a, theta, omega, alpha, y, K, P, xhat, z)
 
 
