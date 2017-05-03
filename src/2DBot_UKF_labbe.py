d63e4f0d28e8da9bcc7b4e2365275bb2de5f55c6
Jelle Spijker <spijker.jelle@gmail.com>
2017-05-03 20:10:24 +0200

    UKF implemented

--- src/2DBot_UKF_labbe.py
+++ src/2DBot_UKF_labbe.py
@@ -1,8 +1,10 @@
-import matplotlib.pyplot as pl
-
-pl.rcParams['legend.loc'] = 'best'
+from filterpy.kalman import UnscentedKalmanFilter as UKF
+from filterpy.kalman import MerweScaledSigmaPoints
+from filterpy.common import Q_discrete_white_noise
+from numpy import random, dot
+from numpy.random import randn
 import numpy as np
-from numpy import dot
+import matplotlib.pyplot as plt
 from scipy.linalg import inv
 
 
@@ -72,17 +74,17 @@ def build_measurement_values(t, real_values):
     drift = np.ones((real_values[1].size,)) * np.random.normal(drift_mu, drift_sigma)
     omega_m += drift
 
-    z = np.zeros((t.size, 3, 1))
-    z[:, 0] = np.reshape(a_m[:, 0], (t.size, 1))
-    z[:, 1] = np.reshape(a_m[:, 1], (t.size, 1))
-    z[:, 2] = np.reshape(omega_m, (t.size, 1))
+    z = np.zeros((t.size, 3, ))
+    z[:, 0] = np.reshape(a_m[:, 0], (t.size, ))
+    z[:, 1] = np.reshape(a_m[:, 1], (t.size, ))
+    z[:, 2] = np.reshape(omega_m, (t.size, ))
     return z
 
 
 def build_control_values(t, real_values):
-    u = np.zeros((t.size, 2, 1))
-    u[:, 0] = np.reshape(real_values[:, 0], (t.size, 1))
-    u[:, 1] = np.reshape(real_values[:, 1], (t.size, 1))
+    u = np.zeros((t.size, 2, ))
+    u[:, 0] = np.reshape(real_values[:, 0], (t.size, ))
+    u[:, 1] = np.reshape(real_values[:, 1], (t.size, ))
     return u
 
 
@@ -123,7 +125,7 @@ def init_kalman(t, dt):
         [0., 0., 0.],
         [0., 0., 1.],
         [0., 0., 0]
-    ])
+    ]).T
 
     Q = np.array([
         [0.05 * dt ** 5, 0., 0.1666 * dt ** 3, 0., 0., 0., 0., 0., 0.],
@@ -142,52 +144,52 @@ def init_kalman(t, dt):
         [0., 9e-1, 0.],
         [0., 0., 2.e-3],
     ]) * 100
-    v = np.random.normal(0, 25e-1, (t.size, 3, 1)) * 0
-    w = np.random.normal(0, 25e-1, (t.size, 9, 1)) * 0
-    return [F, B, H, Q, R, v, w]
+    return [F, B, H, Q, R]
 
 
-def kalman(t, kalman_values, u, z, error):
-    x = np.zeros((t.size, 9, 1))
-    P = np.zeros((t.size, 9, 9))
-    P[0, :, :] = np.array([
-        [error[0] ** 2, 0., 0., 0., 0., 0., 0., 0., 0.],
-        [0., error[0] ** 2, 0., 0., 0., 0., 0., 0., 0.],
-        [0., 0., error[1] ** 2, 0., 0., 0., 0., 0., 0.],
-        [0., 0., 0., error[1] ** 2, 0., 0., 0., 0., 0.],
-        [0., 0., 0., 0., error[2] ** 2, 0., 0., 0., 0.],
-        [0., 0., 0., 0., 0., error[2] ** 2, 0., 0., 0.],
-        [0., 0., 0., 0., 0., 0., error[3] ** 2, 0., 0.],
-        [0., 0., 0., 0., 0., 0., 0., error[4] ** 2, 0.],
-        [0., 0., 0., 0., 0., 0., 0., 0., error[5] ** 2]
-    ])
+def construct_xground(s, v, a, theta, omega, alpha, shape):
+    xground = np.zeros(shape)
+    xground[:, 0] = s[:, 0].reshape(s[:, 0].size, )
+    xground[:, 3] = s[:, 1].reshape(s[:, 1].size, )
+    xground[:, 1] = v[:, 0].reshape(v[:, 0].size, )
+    xground[:, 4] = v[:, 1].reshape(v[:, 1].size, )
+    xground[:, 2] = a[:, 0].reshape(a[:, 0].size, )
+    xground[:, 5] = a[:, 1].reshape(a[:, 1].size, )
+    xground[:, 6] = theta.reshape(theta.size, )
+    xground[:, 7] = omega.reshape(omega.size, )
+    xground[:, 8] = alpha.reshape(alpha.size, )
+    return xground
 
-    xhat = np.zeros((t.size, 9, 1))
-    y = np.zeros((t.size, 3, 1))
 
-    F = kalman_values[0]
-    B = kalman_values[1]
-    H = kalman_values[2]
-    Q = kalman_values[3]
-    R = kalman_values[4]
-    v = kalman_values[5]
-    w = kalman_values[6]
+def plot_results(t, x, xground, z, nees):
+    plt.figure(0)
+    plt.grid(True)
+    plt.plot(xground[:, 0], xground[:, 3], '.', label='s')
+    plt.plot(x[:, 0], x[:, 3], 'x', label='x')
+    plt.ylabel('y [m]')
+    plt.xlabel('x [m]')
+    plt.legend()
+    plt.show()
 
-    K = np.zeros((t.size, 9, 3))
-
-    for k in range(1, t.size):
-        xhat[k] = dot(F, x[k - 1]) + dot(B, u[k]) + w[k]
-        Phat = dot(F, dot(P[k - 1], F.T)) + Q
-
-        y[k] = z[k] - dot(H.T, xhat[k]) + v[k]
-
-        S = dot(H.T, dot(Phat, H)) + R
-        S = np.linalg.inv(S)
-        K[k] = dot(Phat, dot(H, S))
-        x[k] = xhat[k] + dot(K[k], y[k])
-
-        P[k] = dot(np.eye(9) - dot(K[k], H.T), Phat)
-    return [x, K, P, xhat, y]
+    plt.figure(1)
+    plt.subplot(311)
+    plt.plot(t, xground[:, 2], label='a')
+    plt.plot(t, x[:, 2], 'x', label='x')
+    plt.plot(t, z[:, 0], '.', label='y')
+    plt.xlabel('Time [s]')
+    plt.ylabel('A_x [m/s^2]')
+    plt.legend()
+    plt.subplot(312)
+    plt.plot(t, xground[:, 1], label='v')
+    plt.plot(t, x[:, 1], 'x', label='x')
+    plt.xlabel('Time [s]')
+    plt.ylabel('v_x [m/s]')
+    plt.legend()
+    plt.subplot(313)
+    plt.plot(t, nees, label='NEES')
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
 
 
 def NEES(xs, est_xs, ps):
@@ -200,77 +202,73 @@ def NEES(xs, est_xs, ps):
     return err
 
 
-def plot_results(t, x, xground, y, K, P, xhat, z, nees):
-    pl.figure(0)
-    pl.grid(True)
-    pl.plot(xground[:, 0], xground[:, 3], '.', label='s')
-    pl.plot(x[:, 0], x[:, 3], 'x', label='x')
-    pl.ylabel('y [m]')
-    pl.xlabel('x [m]')
-    pl.legend()
-    pl.show()
+def f_bot(x, dt, u):
+    F = np.array([
+        [1., 0., 0.5 * dt ** 2, 0., 0., 0., 0., 0., 0.],
+        [0., 0., dt, 0., 0., 0., 0., 0., 0.],
+        [0., 0., 1., 0., 0., 0., 0., 0., 0.],
+        [0., 0., 0., 1., 0., 0.5 * dt ** 2, 0., 0., 0.],
+        [0., 0., 0., 0., 0., dt, 0., 0., 0.],
+        [0., 0., 0., 0., 0., 1., 0., 0., 0., ],
+        [0., 0., 0., 0., 0., 0., 1., dt, 0.5 * dt ** 2],
+        [0., 0., 0., 0., 0., 0., 0., 1., dt],
+        [0., 0., 0., 0., 0., 0., 0., 0., 1.]
+    ])
+    B = np.array([
+        [dt, 0.],
+        [1., 0.],
+        [0., 0.],
+        [0., dt],
+        [0., 1.],
+        [0., 0.],
+        [0., 0.],
+        [0., 0.],
+        [0., 0.]
+    ])
+    return dot(F, x) + dot(B, u)
 
-    pl.figure(1)
-    pl.subplot(311)
-    pl.plot(t, xground[:, 2], label='a')
-    pl.plot(t, x[:, 2], 'x', label='x')
-    pl.plot(t, z[:, 0], '.', label='y')
-    pl.xlabel('Time [s]')
-    pl.ylabel('A_x [m/s^2]')
-    pl.legend()
-    pl.subplot(312)
-    pl.plot(t, xground[:, 1], label='v')
-    pl.plot(t, x[:, 1], 'x', label='x')
-    pl.xlabel('Time [s]')
-    pl.ylabel('v_x [m/s]')
-    pl.legend()
-    pl.subplot(313)
-    pl.plot(t, nees, label='NEES')
-    pl.legend()
-    pl.tight_layout()
-    pl.show()
 
-def save_results(t, xground, x, nees):
-    # save csv
-    datPos = np.zeros((t.size, 4, 1))
-    datPos[:, 1] = xground[:, 0]
-    datPos[:, 2] = xground[:, 3]
-    datPos[:, 2] = x[:, 0]
-    datPos[:, 3] = x[:, 3]
-    np.savetxt('2DBotPos.dat', datPos, delimiter=',')
-
-    datNEES = np.zeros((t.size, 2, 1))
-    datNEES[:, 0] = t.reshape((t.size, 1))
-    datNEES[:, 1] = nees.reshape(t.size, 1)
-    np.savetxt('2DBot_NEES.dat', datNEES)
-
-def construct_xground(s, v, a, theta, omega, alpha, shape):
-    xground = np.zeros(shape)
-    xground[:, 0] = s[:, 0].reshape(s[:, 0].size, 1)
-    xground[:, 3] = s[:, 1].reshape(s[:, 1].size, 1)
-    xground[:, 1] = v[:, 0].reshape(v[:, 0].size, 1)
-    xground[:, 4] = v[:, 1].reshape(v[:, 1].size, 1)
-    xground[:, 2] = a[:, 0].reshape(a[:, 0].size, 1)
-    xground[:, 5] = a[:, 1].reshape(a[:, 1].size, 1)
-    xground[:, 6] = theta.reshape(theta.size, 1)
-    xground[:, 7] = omega.reshape(omega.size, 1)
-    xground[:, 8] = alpha.reshape(alpha.size, 1)
-    return xground
+def h_bot(x):
+    H = np.array([
+        [0., 0., 0.],
+        [0., 0., 0.],
+        [1., 0., 0.],
+        [0., 0., 0.],
+        [0., 0., 0.],
+        [0., 1., 0.],
+        [0., 0., 0.],
+        [0., 0., 1.],
+        [0., 0., 0]
+    ]).T
+    return dot(H, x)
 
 
 def main():
     [t, dt, s, v, a, theta, omega, alpha] = build_real_values()
-    z = build_measurement_values(t, [a, omega])
+    zs = build_measurement_values(t, [a, omega])
     u = build_control_values(t, v)
-    [F, B, H, Q, R, vv, w] = init_kalman(t, dt)
-    error = [5, 0.05, 0.5, 0.5, 0.2, 0.4]
-    kalman_values = [F, B, H, Q, R, vv, w]
-    x, K, P, xhat, y = kalman(t, kalman_values, u, z, error)
-    xground = construct_xground(s, v, a, theta, omega, alpha, x.shape)
-    nees = NEES(xground, x, P)
+    [F, B, H, Q, R] = init_kalman(t, dt)
+
+    sigmas = MerweScaledSigmaPoints(n=9, alpha=.1, beta=2., kappa=-1)
+    kf = UKF(dim_x=9, dim_z=3, fx=f_bot, hx=h_bot, dt=0.2, points=sigmas)
+    kf.x = np.array([0., 0., 0., 0., 0., 0., 0., 0., 0.])
+    kf.R = R
+    kf.F = F
+    kf.H = H
+    kf.Q = Q
+
+    xs, cov = [], []
+    for zk, uk in zip(zs, u):
+        kf.predict(fx_args=uk)
+        kf.update(z=zk)
+        xs.append(kf.x.copy())
+        cov.append(kf.P)
+
+    xs, cov = np.array(xs), np.array(cov)
+    xground = construct_xground(s, v, a, theta, omega, alpha, xs.shape)
+    nees = NEES(xground, xs, cov)
     print(np.mean(nees))
-    save_results(t, xground, x, nees)
-    plot_results(t, x, xground, y, K, P, xhat, z, nees)
+    plot_results(t, xs, xground, zs, nees)
 
 
 if __name__ == '__main__':
